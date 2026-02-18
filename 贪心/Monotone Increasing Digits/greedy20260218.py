fe7a5aefdc5b3f9d6e1961f890ea71faf16a24d0
Althea Lam <althealam0531@gmail.com>
2026-02-18 10:59:48 -0500

    commit

---
+++ 贪心/Monotone Increasing Digits/greedy20260218.py
@@ -0,0 +1,13 @@
+class Solution:
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        str_n = list(str(n))
+        for i in range(len(str_n)-1, 0, -1):
+            if int(str_n[i-1])>int(str_n[i]):
+                str_n[i-1] = str(int(str_n[i-1])-1)
+                str_n[i:] = '9'*(len(str_n)-i)
+        return int(''.join(str_n))
+
+n = 332
+sol = Solution()
+res = sol.monotoneIncreasingDigits(n)
+print(res)
