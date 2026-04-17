078608cbabc31ae888b8f67d1450e3473e28dc25
Althea Lam <althealam0531@gmail.com>
2026-04-16 21:48:20 -0400

    commit

--- Tree/Binary Tree (Post Order Practice)/*Minimum Time to Collect All Apples in a Tree/dfs20260416.py
+++ Tree/Binary Tree (Post Order Practice)/*Minimum Time to Collect All Apples in a Tree/dfs20260416.py
@@ -7,8 +7,6 @@
 # - 如果dfs(child)>0说明子树深度有苹果，当前节点到孩子的这条边必须走，因此+2
 # - 如果dfs(child)=0，但是hasApple[child]=True，说明孩子本人为苹果，因此+2
 
-# 注意：edges是无向边，并不是代表第一个元素一定是父亲节点，第二个元素一定是孩子节点
-# 一定要在dfs里使用parentidx，避免无限循环，因此graph[parent]有children，graph[children]也有parent，因此遍历graph的时候会导致重复
 
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
@@ -24,6 +22,8 @@ class Solution:
     def dfs(self, nodeidx, parentidx, graph, hasApple):
         total_time = 0
         for child in graph[nodeidx]:
+            # 比如节点0有孩子节点1，因此dfs(0, 1)
+            # 调用dfs(0, 1)的时候，发现1有孩子节点0，但是0和父亲节点一样，所以不会出现回流
             if child==parentidx: # 防止回流
                 continue
             child_time = self.dfs(child, nodeidx, graph, hasApple)
